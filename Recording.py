c55de8a5e611157649d0e7b556735dd100380dba
TripleAVerAlpha <TripleAVerAlpha@gmail.com>
2021-11-19 12:17:18 +0300

    add Code

--- Recording.py
+++ Recording.py
@@ -1,18 +1,97 @@
+import json
+import random
+import subprocess
+import soundfile as sf
 from TelegramBot import Bot
 import pandas as pd
 import os
 
+param_file = "Data/param.json"
+
 
 def get_message(message):
+    global manifest, param
     if "+" in message.text:
         text = message.text.replace('+ ', '')
-        b.send_message(f"Добавляю: {text}")
+        manifest = manifest.append({"Текст": text}, ignore_index=True)
+        manifest.to_csv("Data/manifest.csv")
+        bot.send_message(f"Добавляю: {text}")
+
+    if "Запись" in message.text:
+        param["record"] = True
+        bot.send_message(f"Запись включина", ["Конец"])
+        nextString()
+
+    if "Конец" in message.text:
+        param["record"] = False
+        bot.send_message(f"Запись выключина", ["Запись", "Статистика"])
+
+    if "Пропустить" in message.text:
+        nextString()
+
+    if "Статистика" in message.text:
+        print(manifest)
+        d = 0
+        for i in manifest["Файл"].dropna(axis=0).index:
+            if manifest.loc[i, "Длительность"] == 0:
+                audio, sample_rate = sf.read(manifest.loc[i, "Файл"] + ".wav")
+                manifest.loc[i, "Длительность"] = len(audio) / sample_rate
+            d += manifest.loc[i, "Длительность"]
+        text = ""
+        stat = manifest.groupby("Текст").count()
+        for i in stat.index:
+            text += f"{i} -  {stat.loc[i, 'Файл']}\n"
+
+        h = d // 3600
+        m = d % 3600 // 60
+        s = d % 3600 % 60
+        text += f"Всего: {h} часов {m} минут {s} секунд"
+        bot.send_message(text)
 
 
-if os.path.exists("model/line.csv"):
-    all_data = pd.read_csv("model/line.csv", index_col=0)
+def record(message):
+    global manifest, param
+    if param["record"]:
+        src_filename = f"Data/Запись {len(manifest)}.ogg"
+        dest_filename = f"Data/WAV/Запись {len(manifest)}.wav"
+        bot.get_voice(message, src_filename)
+        outfile = open("out.txt", "w+")
+        process = subprocess.run(['ffmpeg', "-i", src_filename, dest_filename], stderr=outfile)
+        if process.returncode != 0:
+            raise Exception("Something went wrong")
+        else:
+            row = {"Текст": param["record_text"], "Файл": dest_filename.replace(".wav", ""), "Длительность": 0}
+            manifest = manifest.append(row, ignore_index=True)
+            manifest.to_csv("Data/manifest.csv")
+            nextString()
+        os.remove(src_filename)
+
+
+def nextString():
+    global param
+    stat = manifest.groupby("Текст").count().index
+    text = stat[random.randint(0, len(stat)-1)]
+    param["record_text"] = text
+    bot.send_message(text, ["Удалить пр запись", "Пропустить", "Конец"])
+
+
+if os.path.exists(param_file):
+    with open(param_file, "r") as f:
+        param = json.load(f)
 else:
-    all_data = pd.DataFrame(columns=["Текст", "Файл"])
-b = Bot(file="Data/setting")
-b.registration_message_reader(get_message)
-b.start()
+    param = {
+        "record": False,
+        "record_text": ""
+    }
+if os.path.exists("Data/manifest.csv"):
+    manifest = pd.read_csv("Data/manifest.csv", index_col=0)
+else:
+    manifest = pd.DataFrame(columns=["Текст", "Файл"])
+print(manifest)
+
+bot = Bot(file="Data/setting")
+bot.send_message("Начинаю работу", ["Запись", "Статистика"])
+bot.del_message_reader()
+bot.registration_message_reader(record, content_types=["voice"])
+bot.registration_message_reader(get_message, content_types=["text"])
+bot.start()
--- TelegramBot.py
+++ TelegramBot.py
@@ -1,4 +1,6 @@
+import requests
 import telebot
+from telebot import types
 
 
 class BotError(Exception):
@@ -26,13 +28,18 @@ class Bot:
         if token == "":
             raise BotError("Токен пустой")
 
+        self.token = token
         self.user_id = id
         self.bot = telebot.TeleBot(token)
         self.registration_message_reader(self.get_message)
+        self.menu = []
+
+    def del_message_reader(self):
+        self.bot.message_handlers = []
 
     def registration_message_reader(self, func, content_types=['text']):
-        self.bot.message_handlers = []
         self.bot.add_message_handler({'function': func, 'filters': {'content_types': content_types}})
+        print(self.bot.message_handlers)
 
     def get_message(self, message):
         work = True
@@ -44,7 +51,19 @@ class Bot:
         if work:
             self.send_message(message.text)
 
-    def send_message(self, text, id=-1, message=None):
+    def getKeyboard(self, button_=None):
+        if button_ is None:
+            button_ = self.menu
+        # Создание клавиатуры для ответов
+        keyboard = types.ReplyKeyboardMarkup()
+        try:
+            for i in button_:
+                keyboard.add(types.KeyboardButton(text=i))
+        except TypeError:
+            keyboard = types.ReplyKeyboardRemove()
+        return keyboard
+
+    def send_message(self, text, button_=None, id=-1, message=None):
         if message is None:
             if id == -1:
                 if self.user_id == -1:
@@ -54,7 +73,17 @@ class Bot:
         else:
             id = message.from_user.id
 
-        self.bot.send_message(id, text)
+        if button_ is None:
+            self.bot.send_message(id, text)
+        else:
+            keybord = self.getKeyboard(button_)
+            self.bot.send_message(id, text, reply_markup=keybord)
+
+    def get_voice(self, message, path):
+        file_info = self.bot.get_file(message.voice.file_id)
+        file_ = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(self.token, file_info.file_path)).content
+        with open(path, "wb") as open_file:
+            open_file.write(file_)
 
     def start(self):
         self.bot.polling()
